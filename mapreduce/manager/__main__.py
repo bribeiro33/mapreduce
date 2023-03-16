f294679bf4b8aadf3a403f7afd5330b7b51064e3
Barbara Dreyer Rentz Ribeiro <53235549+bribeiro33@users.noreply.github.com>
2023-03-15 23:57:05 -0400

    Passing shutdown and registration

--- mapreduce/manager/__main__.py
+++ mapreduce/manager/__main__.py
@@ -1,5 +1,7 @@
 """MapReduce framework Manager node."""
 import os
+import socket
+import threading
 import tempfile
 import logging
 import json
@@ -11,31 +13,129 @@ import mapreduce.utils
 # Configure logging
 LOGGER = logging.getLogger(__name__)
 
+class WTag:
+    """A class representing a Worker node's identifying info."""
 
+    # TODO: add job parameter later as needed
+    def __init__(self, host_in, port_in, state_in):
+        """Construct a WTag instance"""
+        self.host = host_in
+        self.port = port_in
+        self.state = state_in
+    
 class Manager:
     """Represent a MapReduce framework Manager node."""
 
     def __init__(self, host, port):
         """Construct a Manager instance and start listening for messages."""
-
+        ## commented out pwd bc it's long
         LOGGER.info(
-            "Starting manager host=%s port=%s pwd=%s",
-            host, port, os.getcwd(),
+            "Starting manager:%s", port,
         )
-
-        # This is a fake message to demonstrate pretty printing with logging
-        message_dict = {
-            "message_type": "register",
-            "worker_host": "localhost",
-            "worker_port": 6001,
+        LOGGER.info(
+            "PWD",
+        )
+        # Member vars
+        self.options = {
+            "host": host, 
+            "port": port
         }
-        LOGGER.debug("TCP recv\n%s", json.dumps(message_dict, indent=2))
+        self.shutdown = False
+        self.workers = {}
+        
+        self.tcp_thread = threading.Thread(target=self.tcp_listening)
+        self.tcp_thread.start()
+        self.tcp_thread.join()
+        self.shutdown_workers()
 
-        # TODO: you should remove this. This is just so the program doesn't
-        # exit immediately!
-        LOGGER.debug("IMPLEMENT ME!")
-        time.sleep(120)
+    def tcp_listening(self):
+        """Wait on a message from a socket OR a shutdown signal."""
+        # Create an INET, STREAMing socket, this is TCP
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            # Bind the socket to the server
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            sock.bind((self.options["host"], self.options["port"]))
+            sock.listen()
+            sock.settimeout(1)
 
+            while not self.shutdown:
+                # Wait for a connection for 1s.  The socket library avoids
+                # consuming CPU while waiting for a connection.
+                try:
+                    clientsocket, address = sock.accept()
+                except socket.timeout:
+                    continue
+
+                # Socket recv() will block for a maximum of 1 second.  If you omit
+                # this, it blocks indefinitely, waiting for packets.
+                clientsocket.settimeout(1)
+
+                with clientsocket:
+                    message_chunks = []
+                    while True:
+                        try:
+                            data = clientsocket.recv(4096)
+                        except socket.timeout:
+                            continue
+                        if not data:
+                            break
+                        message_chunks.append(data)
+
+                # Decode list-of-byte-strings to UTF8 and parse JSON data
+                message_bytes = b''.join(message_chunks)
+                message_str = message_bytes.decode("utf-8")
+
+                try:
+                    message_dict = json.loads(message_str)
+                except json.JSONDecodeError:
+                    continue
+                # Handle message 
+                if message_dict["message_type"] == "shutdown":
+                    # if shutdown message has been received, will not go back into 
+                    # while loop and will effectively, well, shutdown
+                    self.shutdown = True
+                    LOGGER.debug("recieved\n%s", json.dumps(message_dict, indent=2))
+                    LOGGER.info("shutting down")
+
+                if message_dict["message_type"] == "register":
+                    # Worker is ready to listen
+                    LOGGER.debug(
+                        "recieved\n%s",
+                        json.dumps(message_dict, indent=2),
+                    )
+                    # Add worker to worker list
+                    new_worker = WTag(
+                        host_in=message_dict["worker_host"], 
+                        port_in=message_dict["worker_port"], 
+                        state_in="ready"
+                        #job
+                    )
+                    self.workers[(new_worker.host, new_worker.port)] = new_worker
+                    LOGGER.info("registered worker %s:%s",
+                        message_dict["worker_host"], message_dict["worker_port"],
+                    )
+                    # Send acknolegement to worker
+                    self.send_ack(message_dict["worker_host"], message_dict["worker_port"])
+
+    def send_ack(self, worker_host, worker_port):
+        """Send the worker a registration ack."""
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.connect((worker_host, worker_port))
+            reg_ack_message = json.dumps({
+                "message_type": "register_ack",
+                "worker_host": worker_host,
+                "worker_port": worker_port
+            })
+            sock.sendall(reg_ack_message.encode('utf-8'))
+
+    def shutdown_workers(self):
+        """Forward the shutdown message to all the workers."""
+        for worker in self.workers.values():
+            if worker.state != "dead":
+                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+                    sock.connect((worker.host, worker.port))
+                    shutdown_message = json.dumps({"message_type": "shutdown"})
+                    sock.sendall(shutdown_message.encode('utf-8'))
 
 @click.command()
 @click.option("--host", "host", default="localhost")
--- mapreduce/worker/__main__.py
+++ mapreduce/worker/__main__.py
@@ -4,38 +4,157 @@ import logging
 import json
 import time
 import click
+import socket
+import string
+import threading
 import mapreduce.utils
 
 
 # Configure logging
 LOGGER = logging.getLogger(__name__)
 
-
 class Worker:
     """A class representing a Worker node in a MapReduce cluster."""
     def __init__(self, host, port, manager_host, manager_port):
         """Construct a Worker instance and start listening for messages."""
         LOGGER.info(
-            "Starting worker host=%s port=%s pwd=%s",
-            host, port, os.getcwd(),
+            "Starting worker:%s", port,
         )
-        LOGGER.info(
-            "manager_host=%s manager_port=%s",
-            manager_host, manager_port,
-        )
+        LOGGER.info("PWD",)
 
-        # This is a fake message to demonstrate pretty printing with logging
-        message_dict = {
-            "message_type": "register_ack",
-            "worker_host": "localhost",
-            "worker_port": 6001,
+        # Member vars
+        self.state = "ready"
+        self.shutdown = False
+        self.options= {
+            "host": host, 
+            "port": port,
+            "manager_host": manager_host,
+            "manager_port": manager_port,
         }
-        LOGGER.debug("TCP recv\n%s", json.dumps(message_dict, indent=2))
+        self.hbthread = threading.Thread(target=self.send_heartbeat)
 
-        # TODO: you should remove this. This is just so the program doesn't
-        # exit immediately!
-        LOGGER.debug("IMPLEMENT ME!")
-        time.sleep(120)
+        self.register()
+        self.tcp_listening()
+
+
+    # function to access the worker's state variable from manager
+    def get_state(self): 
+        return self.state
+    
+    def get_workerhost(self):
+        return self.options["host"]
+    
+    def get_workerport(self):
+        return self.options["port"]
+
+    def register(self):
+        """Send registration message when worker is ready to Manager."""
+        # create an INET, STREAMing socket, this is TCP
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+
+            # connect to the server
+            sock.connect((self.options["manager_host"], self.options["manager_port"]))
+
+            # send a message when worker is ready
+            if (self.state == "ready"):
+                # this is the message
+                message_dict = {
+                    "message_type" : "register",
+                    "worker_host" : self.options["host"],
+                    "worker_port" : self.options["port"],
+                }
+                message = json.dumps(message_dict)
+                # send the message
+                sock.sendall(message.encode('utf-8'))
+    
+    def send_heartbeat(self):
+        """Send heartbeat once worker has been registered with manager."""
+
+        # check if shutdown
+        while not self.shutdown:
+            # Create an INET, DGRAM socket, this is UDP
+            with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+
+                # Connect to the UDP socket on server
+                sock.connect((self.options["manager_host"], self.options["manager_port"]))
+
+                # Send a hb message
+                message_dict = {
+                    "message_type": "heartbeat",
+                    "worker_host": self.options["host"],
+                    "worker_port": self.options["port"],
+                }
+                message = json.dumps(message_dict)
+                # send the message
+                sock.sendall(message.encode('utf-8'))
+            time.sleep(2)
+    
+    def tcp_listening(self):
+        """Listen for incoming messages from manager."""
+        # Create an INET, STREAMing socket, this is TCP
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            # Bind the socket to the server
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            sock.bind((self.options["host"], self.options["port"]))
+            sock.listen()
+            sock.settimeout(1)
+
+            while not self.shutdown:
+                
+                # Wait for a connection for 1s.  The socket library avoids
+                # consuming CPU while waiting for a connection.
+                try:
+                    clientsocket, address = sock.accept()
+                except socket.timeout:
+                    continue
+
+                # Socket recv() will block for a maximum of 1 second.  If you omit
+                # this, it blocks indefinitely, waiting for packets.
+                clientsocket.settimeout(1)
+
+                with clientsocket:
+                    message_chunks = []
+                    while True:
+                        try:
+                            data = clientsocket.recv(4096)
+                        except socket.timeout:
+                            continue
+                        if not data:
+                            break
+                        message_chunks.append(data)
+
+                # Decode list-of-byte-strings to UTF8 and parse JSON data
+                message_bytes = b''.join(message_chunks)
+                message_str = message_bytes.decode("utf-8")
+
+                try:
+                    message_dict = json.loads(message_str)
+                except json.JSONDecodeError:
+                    continue
+
+                # Handle message & threads
+
+                # threads = []
+                if message_dict["message_type"] == "shutdown":
+                    # if shutdown message has been received, will not go back into 
+                    # while loop and will effectively, well, shutdown
+                    self.shutdown = True
+                    
+                    LOGGER.debug("recieved\n%s", json.dumps(message_dict, indent=2))
+                    LOGGER.info("shutting down",)
+                    # print("server() shutting down")
+                if message_dict["message_type"] == "register_ack":
+                    # manager has receieved worker registration
+                    LOGGER.debug(
+                       "recieved\n%s", json.dumps(message_dict, indent=2),
+                    )
+                    # create thread
+                    # threads.append(thread)
+                    self.hbthread.start()  
+            
+            if self.hbthread.is_alive():
+                self.hbthread.join()            
+
 
 
 @click.command()
